0b46c1d68f998de708e0ab724ea538f364bffcf1
yatharthranjan <yatharthranjan89@gmail.com>
2021-03-04 17:10:06 +0000

    Add test cases for overflowing reservoir(20,000 samples)

--- src/test/java/org/radarbase/ksql/udaf/UdafTest.java
+++ src/test/java/org/radarbase/ksql/udaf/UdafTest.java
@@ -2,17 +2,22 @@ package org.radarbase.ksql.udaf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.radarbase.ksql.udaf.UniformSamplingReservoirUdaf.COUNT;
 import static org.radarbase.ksql.udaf.UniformSamplingReservoirUdaf.SAMPLES;
 
 import io.confluent.ksql.function.udaf.Udaf;
 import java.util.ArrayList;
+import java.util.Random;
+import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.util.Precision;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("PMD.DataflowAnomalyAnalysis")
 public class UdafTest {
 
     private static final Double[] values = new Double[]{40.0, 50.0, 60.0};
@@ -96,5 +101,54 @@ public class UdafTest {
         struct = iqrUdaf.aggregate(70.0, struct);
 
         assertEquals(25.0, iqrUdaf.map(struct));
+    }
+
+    @Test
+    @SuppressWarnings("PMD.AvoidLiteralsInIfCondition")
+    void overflowingReservoirModeTest() {
+        Udaf<Double, Struct, Double> modeUdaf = ModeUdaf.createUdaf();
+
+        // Add 6000 values for 40.0
+        // Add 7000 values for 60.0
+        // Add 8000 values for 50.0
+        int n = 8_000;
+        while (n > 0) {
+            if (n < 6000) {
+                struct = modeUdaf.aggregate(40.0, struct);
+            }
+            if (n < 7000) {
+                struct = modeUdaf.aggregate(60.0, struct);
+            }
+            struct = modeUdaf.aggregate(50.0, struct);
+            n--;
+        }
+
+        // So mode should be 50 if the distribution is accurately estimated in the reservoir
+        assertEquals(1000, struct.getArray(SAMPLES).size());
+        assertEquals(50.0, modeUdaf.map(struct));
+    }
+
+    @Test
+    void overflowingReservoirStdDevTest() {
+        Udaf<Double, Struct, Double> stdDevUdaf = StdDevUdaf.createUdaf();
+        Random rand = new Random();
+        int n = 20_000;
+
+        double[] vals = new double[20_000];
+        for (int i = 0; i < n; i++) {
+            double value = values[rand.nextInt(values.length)];
+            struct = stdDevUdaf.aggregate(value, struct);
+            vals[i] = value;
+        }
+
+        assertEquals(5000, struct.getArray(SAMPLES).size());
+
+        double stdDev = stdDevUdaf.map(struct);
+        double expectedStdDev = getExpectedStdDev(vals);
+        assertTrue(Precision.equals(expectedStdDev, stdDev, 0.1));
+    }
+
+    double getExpectedStdDev(double[] vals) {
+        return Math.sqrt(StatUtils.variance(vals));
     }
 }
